c2aa6438952b5e0f63aec4aa624304b1a64f4b32
Rozhak <65714340+RozhakDev@users.noreply.github.com>
2026-06-04 01:01:24 +0700

    feat(database): implement core schema and relations

---
+++ app/Models/Event.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'event_date',
+        'location',
+        'quota',
+    ];
+
+    protected $casts = [
+        'event_date' => 'datetime',
+    ];
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class);
+    }
+}
